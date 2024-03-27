a9cc014e7dae186dd83835eacb8054270f6fd491
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-27 14:12:42 +0100

    add title and img

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 import {getRndInteger} from "./utility.js";
 // import getRndInteger from "./utility.js";
+
+const { createApp } = Vue
+
+const app =createApp({
+    data(){
+        return{
+            message:'Hello Word',
+            image: 'https://www.dequo.it/articoli/app/uploads/2023/05/copyright-immagini-generate-ai12.jpg'
+        }
+    }
+}).mount('#app');
