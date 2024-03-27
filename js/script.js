0d658367ac64e956ceb71559c6ae495f6663550c
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-27 14:28:16 +0100

    responsive img

--- js/script.js
+++ js/script.js
@@ -7,7 +7,21 @@ const app =createApp({
     data(){
         return{
             message:'Hello Word',
-            image: 'https://www.dequo.it/articoli/app/uploads/2023/05/copyright-immagini-generate-ai12.jpg'
+            image: 'https://www.dequo.it/articoli/app/uploads/2023/05/copyright-immagini-generate-ai12.jpg',
+            miaClasse:'red'
+        }
+    },
+    methods:{
+        toggleClass(){
+            this.miaClasse= this.miaClasse === 'red' ? 'blue' :'red';
+        },
+        toggleImg(){
+            this.image = this.image === 'https://www.dequo.it/articoli/app/uploads/2023/05/copyright-immagini-generate-ai12.jpg' ? 
+            'https://www.ideabit.com/album/foto_full/formato-immagini-webp_710.jpg' : 'https://www.dequo.it/articoli/app/uploads/2023/05/copyright-immagini-generate-ai12.jpg';
+        },
+        otherImg(){
+            this.image = this.image === 'https://www.dequo.it/articoli/app/uploads/2023/05/copyright-immagini-generate-ai12.jpg' ? 
+            'https://cc-prod.scene7.com/is/image/CCProdAuthor/FF-SEO-text-to-image-marquee-mobile-2x?$pjpeg$&jpegSize=100&wid=600' : 'https://www.dequo.it/articoli/app/uploads/2023/05/copyright-immagini-generate-ai12.jpg';
         }
     }
 }).mount('#app');
